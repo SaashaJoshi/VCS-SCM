fba96a5863ed691a8bb2cf8a73523a067bccffa4
SaashaJoshi <saashajoshi08@gmail.com>
2020-04-07 12:05:13 +0530

    Add 2 files

---
+++ bug_file_branch1.py
@@ -0,0 +1,5 @@
+def branch1print():
+	print('You are in branch1')
+
+if __name__ == '__main__':
+	branch1print
---
+++ bug_file_branch2.py
@@ -0,0 +1,5 @@
+def newfunc():
+	print('This ia new file in branch2')
+
+if __name__ == '__main__':
+	newfunc
