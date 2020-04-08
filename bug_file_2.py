aa2b3bf2f25229a543aaab5847b0563ac0c55b97
SaashaJoshi <saashajoshi08@gmail.com>
2020-04-08 11:15:36 +0530

    Revert "merge branch3"
    This reverts commit b2841f91b69aaaa3d08fe5d4eec29f29dc40232c.

--- bug_file_2.py
+++ bug_file_2.py
@@ -2,7 +2,6 @@ def newfile2():
 	print('This is a new file 2')
 	print('This change is made in branch3')
 	print('New change in master')
-	print('This change is made in branch3')
 
 if __name__ == '__main__':
 	newfile2
